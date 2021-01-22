b31d37a749ac38e322610c1d899cf98fe502b6a3
Eriberto Garcia <65020099+Eribertogcx@users.noreply.github.com>
2021-01-22 08:01:37 +0100

    revert back to previous change

--- get-quote.py
+++ get-quote.py
@@ -6,9 +6,7 @@ def primary():
     quotes = f.readlines()
     f.close()
 
-    last = 13
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    print(quotes[12])
 
 if __name__== "__main__":
   primary()
